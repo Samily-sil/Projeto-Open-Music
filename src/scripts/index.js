a2c35475f622da3731add01f87c5a85f29996a75
Samily <samily.sss2005@gmail.com>
2025-05-17 12:56:32 -0300

    "Tarefa 2 concluída"

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,12 @@
-/* Script Global */
+const priceRange = document.getElementById("price-range");
+
+function updateTrackColor() {
+    const value = priceRange.value;
+    const max = priceRange.max;
+    const percentage = (value / max) * 100;
+    priceRange.style.background = `linear-gradient(to right, var(--brand-1) ${percentage}%, var(--gray-5) ${percentage}%)`;
+}
+
+updateTrackColor();
+
+priceRange.addEventListener("input", updateTrackColor);
