aef923bf7802de2c51c52af0d3fff033ed3082b7
Samily <samily.sss2005@gmail.com>
2025-05-21 11:26:16 -0300

    "Tarefa 3 concluída"

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,38 @@
-const priceRange = document.getElementById("price-range");
+import { applyInputRangeStyle } from "./inputRange.js";
+import { albumList } from "./albumsDatabase.js";
 
-function updateTrackColor() {
-    const value = priceRange.value;
-    const max = priceRange.max;
-    const percentage = (value / max) * 100;
-    priceRange.style.background = `linear-gradient(to right, var(--brand-1) ${percentage}%, var(--gray-5) ${percentage}%)`;
+function createAlbumCard(album) {
+  const card = document.createElement("div");
+  card.classList.add("album"); 
+
+    card.innerHTML = `
+    <img src="${album.img}" alt="${album.title}">
+    <h3 class="music-name">${album.title}</h3>
+    <div class="div-1">
+      <p class="music-singer">${album.band}</p>
+      <span class="music-style">${album.genre}</span>                  
+    </div>
+    <div class="div-2">
+      <h3 class="price">R$ ${parseFloat(album.price).toFixed(2).replace(".", ",")}</h3>
+      <button class="buy-btn">Comprar</button>                  
+    </div>
+  `;
+  return card;
 }
 
-updateTrackColor();
+function renderAlbumCards(albumArray) {
+  const albumContainer = document.querySelector(".albums");
+  albumContainer.innerHTML = "";
 
-priceRange.addEventListener("input", updateTrackColor);
+  albumArray.forEach((album) => {
+    const card = createAlbumCard(album);
+    albumContainer.appendChild(card);
+  });
+}
+
+function routine() {
+  applyInputRangeStyle();
+  renderAlbumCards(albumList)
+}
+
+routine();
---
+++ src/scripts/inputRange.js
@@ -0,0 +1,14 @@
+export function applyInputRangeStyle () {
+    const priceRange = document.getElementById("price-range");
+
+    function updateTrackColor() {
+        const value = priceRange.value;
+        const max = priceRange.max;
+        const percentage = (value / max) * 100;
+        priceRange.style.background = `linear-gradient(to right, var(--brand-1) ${percentage}%, var(--gray-5) ${percentage}%)`;
+    }
+
+    updateTrackColor();
+
+    priceRange.addEventListener("input", updateTrackColor);
+}
